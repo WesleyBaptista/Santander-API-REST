70bae247c99a7e8b87e7d2f7ce3314135cb9f17c
Wesley Baptista <wesleybaptista@live.com>
2024-07-28 17:50:03 -0300

    Santandrr-API-REST

---
+++ src/test/java/DIO/WB/Santander_API_REST/SantanderApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO.WB.Santander_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
